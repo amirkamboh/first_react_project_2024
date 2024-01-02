2a17252cdeb2ad7fca62a5e38180bde7a506069e
amirkamboh <amirkamboh036@gmail.com>
2024-01-03 01:10:22 +0500

    updates

---
+++ src/pages/homePage/Components/ourservices.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ourservices = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Ourservices
--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Herosection from './Components/herosection'
+import Ourservices from './Components/ourservices'
 
 const HomePage = () => {
   return (
     <>
         <Herosection />
+        <Ourservices />
     </>
   )
 }
