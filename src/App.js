9952ec7009435e9f611d2e4c4b6a28a8385b3858
amirkamboh <amirkamboh036@gmail.com>
2024-01-06 02:07:02 +0500

    Routes Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import HomePage from "./pages/homePage";
 import Layout from "./globals/Layout";
+import AboutPage from "./pages/aboutUs";
+import ContactPage from "./pages/contactUs";
+import { Route,Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Layout> 
-       <HomePage />
+      <Layout>
+        <Routes>
+          <Route path='/' exact element={<HomePage />} />
+          <Route path='/about-us' exact element={<AboutPage />} />
+          <Route path='/contact-us' exact element={<ContactPage />} />
+          </Routes> 
       </Layout>
       
     </div>
