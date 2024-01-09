0ec34b652bc437a04e30fe0a38c6fc2dd0806fd6
amirkamboh <amirkamboh036@gmail.com>
2024-01-09 18:25:11 +0500

    Dropdown create

--- src/globals/Layout/header.js
+++ src/globals/Layout/header.js
@@ -12,18 +12,24 @@ const Header = () => {
         <nav>
           <ul>
             <li>
-              <Link to="/">
-                Home
-                </Link>
+              <Link to="/">Home</Link>
             </li>
-            <li>Services</li>
+            <li>Services
+          <ul className='dropdown'>
+             <li>Duct Cleaning</li>
+             <li>AC Services</li>
+             <li>Handyman</li>
+             <li>Plumbing</li>
+             <li>Electrical</li>
+             <li>Painting Service</li>
+             <li>Water Tank Cleaning</li>
+          </ul>
+              </li>
             <li>
-              <Link to="/about-us">
-                About Us
-                </Link>
+              <Link to="/about-us">About Us</Link>
             </li>
             <li>
-              <Link to="/contact-us"> Contact Us </Link>
+              <Link to="/contact-us">Contact Us</Link>
             </li>
             <li className="header-btn">
               +92 3037998345
