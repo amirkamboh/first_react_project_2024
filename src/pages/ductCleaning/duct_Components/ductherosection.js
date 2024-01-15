63e83d147a3f542e91d63d11ba0f4a3e1765e060
amirkamboh <amirkamboh036@gmail.com>
2024-01-15 18:53:32 +0500

    dutcc

--- src/pages/ductCleaning/duct_Components/ductherosection.js
+++ src/pages/ductCleaning/duct_Components/ductherosection.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Ductherosection = () => {
   return (
     <>
-      <div className="duct_hero_section page_witdh">Hero Section</div> 
+      <div className="duct_hero_section page_witdh">
+        <div className="duct_sections">
+          <div className="duct_hero_content">
+            <span>Our Services</span>
+            <h1>AC Duct</h1>
+            <h2>Cleaning Services</h2>
+            <div className="duct_button">
+             <button className='duct_btn1'>Book Now</button>
+             <button className='duct_btn2'>Contact Us</button>
+            </div>
+          </div>
+          <div className="duct_hero_image">2</div>
+        </div>
+        </div> 
     </>
   )
 }
